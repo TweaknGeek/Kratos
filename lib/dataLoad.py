43d08776a6f44b764398ed3d609727380180fb79
TweaknGeek <tweakngeekco@proton.me>
2024-08-01 06:54:32 -0400

    Update dataLoad.py

--- lib/dataLoad.py
+++ lib/dataLoad.py
@@ -1,32 +1,59 @@
 import torch
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch import nn, optim
+
+from lossFunc import advanced_loss_function
+from train import train_model
+import model 
+import eval
 
 
-# Define transformations for the CelebA dataset
+# Define the training configuration
+batch_size = 32
+num_epochs = 100
+learning_rate = 0.001
+
+# Load the CelebA dataset
 transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.RandomAffine(degrees=10, translate=(0.1, 0.1)),
     transforms.Resize((64, 64)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-# Define the CelebA dataset with the defined transformations
-dataset = datasets.CelebA(root='./data', download=True, split='train', transform=transform)
+trainset = datasets.CelebA(root='./data', download=True, split='train', transform=transform)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
-# Define the data loader
-trainloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+# Define the generator
+generator = nn.Sequential(
+    nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, padding=0),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
+    nn.Tanh()
+)
 
-# Now you can easily use the trainloader to iterate over your data
-for i, data in enumerate(trainloader, 0):
-    inputs, labels = data
-    print(inputs.shape)
+# Train the generator
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(generator.parameters(), lr=learning_rate)
 
-# You can also use the transforms module to apply transformations to images
-# outside of a data loader
-image = datasets.CelebA.get_image_path(dataset.imgs[0])
-image = datasets.CelebA.loader(image)
-image = transform(image)
-print(image.shape)
+for epoch in range(num_epochs):
+    for i, data in enumerate(trainloader, 0):
+        input, target = data
+        input, target = input.to('cuda'), target.to('cuda')
+        optimizer.zero_grad()
+        output = generator(input)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
 
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
